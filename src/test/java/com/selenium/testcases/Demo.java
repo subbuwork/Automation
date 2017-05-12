eb2e6e26446073d6cbaf423e05c7c5962c2c445b
SUBBARAO BANDI <SUBBARAO.BANDI84@GMAIL.COM>
2017-05-12 16:02:26 -0400

    Initial commit

---
+++ src/test/java/com/selenium/testcases/Demo.java
@@ -0,0 +1,15 @@
+package com.selenium.testcases;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by subbu on 5/12/2017.
+ */
+public class Demo {
+
+    @Test
+    public void demo(){
+        System.out.println("Demo test");
+
+    }
+}
